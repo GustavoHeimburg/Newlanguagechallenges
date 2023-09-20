6673618ea0fa98c004a98a81ad308d91430fbb04
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-20 19:56:51 -0300

    Desafios em java

--- src/LerNotas.java
+++ src/LerNotas.java
@@ -4,5 +4,24 @@ public class LerNotas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double total = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite a nota " + (i + 1));
+            double nota = scanner.nextDouble();
+            total += nota * 2;
+        }
+            double media = total / 10;
+
+        System.out.println("A media do aluno é " +media);
+
+        if (media >= 6){
+            System.out.println("O aluno foi aprovado com sucesso!");
+        }else {
+            System.out.println("O aluno foi reprovado!");
+        }
+
+
     }
 }
+
+
