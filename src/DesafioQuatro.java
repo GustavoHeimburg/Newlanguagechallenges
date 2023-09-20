fa643f662fb9e40fed7530dd0f532c34a21341a5
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-20 20:37:16 -0300

    lógica em java

--- src/DesafioQuatro.java
+++ src/DesafioQuatro.java
@@ -4,8 +4,14 @@ public class DesafioQuatro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double notaUm; double notaDois; double notaTres;
+        System.out.println("Digite a nota A: ");
+        double notaA = scanner.nextDouble();
+        System.out.println("Digite a nota B: ");
+        double notaB = scanner.nextDouble();
+        System.out.println("Digite a nota C: ");
+        double notaC = scanner.nextDouble();
 
-
+        double mediaCalc = (notaA * 2 + notaB * 3 + notaC * 5) / 10;
+        System.out.println("A media final é " + mediaCalc);
     }
 }
