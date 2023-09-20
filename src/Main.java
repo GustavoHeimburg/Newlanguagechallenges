ee5cb579c17b46b35f1d384323c92ef0fd45d4bc
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-20 19:38:01 -0300

    Desafios em java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a sua idade:");
+        int days = scanner.nextInt();
+
+        int anos = days / 365;
+        days = days % 365;
+        int meses = days / 30;
+        days = days % 365;
+
+        System.out.println("A idade é: " + anos + " anos, " + meses + " meses e " + days + " dias.");
+    }
+}
