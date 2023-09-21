2b3d95c9b9911c9216efa3e0b51fc2ffd4303865
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-20 21:34:31 -0300

    lógica em java

--- src/DesaafioCinco.java
+++ src/DesaafioCinco.java
@@ -4,5 +4,19 @@ public class DesaafioCinco {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o numeros de horas que trabalhou");
+        float horastrabalhadas = scanner.nextFloat();
+        System.out.println("Digite o valor da hora trabalhada");
+        float valorHora = scanner.nextFloat();
+        System.out.println("DIgite o numero das horas extras");
+        float valorHoraEx = scanner.nextFloat();
+        System.out.println("Digite o valor das horas extras");
+        float valorHoraExd = scanner.nextFloat();
+
+        double remuneracao = (horastrabalhadas + valorHora + valorHoraEx + valorHoraExd);
+        System.out.println("O valor do salario é: " +remuneracao);
+
     }
-}
+    }
+
+
