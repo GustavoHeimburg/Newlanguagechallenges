e31640d565d615c8c0bf0adff3c5dc2d41db515b
GustavoHeimburg <gutogustavogu@gmail.com>
2023-09-26 08:46:11 -0300

    Lógicas Java

--- src/DesafioOito.java
+++ src/DesafioOito.java
@@ -4,30 +4,41 @@ public class DesafioOito {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite seu nome");
+        System.out.println("Digite seu nome:");
         String nome = scanner.nextLine();
-        System.out.println("DIgite sua idade:");
+        System.out.println("Seja bem-vindo " + nome);
+
+        System.out.println("Digite sua idade:");
         int idade = scanner.nextInt();
-        System.out.println("Voce e estudante de Python?");
+        scanner.nextLine();
+
+        System.out.println("Você é um estudante de Python? Digite 1 para Sim e 2 para Não:");
         int estudante = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("Você é um estudante de Python? ");
-        String resposta = scanner.nextLine();
-
-        System.out.println("Quer escolher qual entrada? Digite 1 para padrao e 2 para VIP");
+        System.out.println("Quer escolher qual entrada? Digite 1 para padrão e 2 para VIP:");
         int entrada = scanner.nextInt();
+        scanner.nextLine();
 
-        if (entrada == 1){
-            System.out.println("Sua entrada padrao e R$20" + entrada);
-        }else {
-            entrada == 2
-
+        double valorEntrada = 0;
+        if (entrada == 1) {
+            valorEntrada = 20;
+            System.out.println("Sua entrada padrão é R$" + valorEntrada);
+        } else if (entrada == 2) {
+            valorEntrada = 50;
+            if (estudante == 1) {
+                valorEntrada *= 0.9;
+                System.out.println("Parabéns! Você é um estudante de Python, você ganhou 10% de desconto na entrada VIP.");
+            }
+            System.out.println("Sua entrada VIP é R$" + valorEntrada);
+        } else {
+            System.out.println("Desculpe, não entendi o tipo de entrada que você deseja comprar.");
         }
 
-        if (idade >= 18){
-            System.out.println("Voce e maior de idade, pode comprar ingressos da festa " + idade);
-        }else {
-            System.out.println("Voce é menor de idade " + idade);
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade, pode comprar ingressos da festa.");
+        } else {
+            System.out.println("Você é menor de idade, nao pode comprar o ingresso. a festa e permitida apenas para maiores de 18 anos.");
         }
     }
 }
