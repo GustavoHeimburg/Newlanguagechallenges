f1ecbc66562e207a06aab64ca8d83b0113fdd8b8
GustavoHeimburg <gutogustavogu@gmail.com>
2023-09-26 23:52:59 -0300

    Lógicas Java

---
+++ src/Tempo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Tempo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite algo: ");
+        String input = scanner.nextLine();
+
+        System.out.println("Não tenho tempo para nada!");
+    }
+}
+
