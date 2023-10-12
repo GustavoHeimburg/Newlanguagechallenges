3d5a1cdd15a4fbedcf731246ec2b28a6f4b68b77
GustavoHeimburg <gutogustavogu@gmail.com>
2023-10-12 10:11:41 -0300

    Logicazinha pra nao perder costume

--- src/CursoJava/idade.java
+++ src/CursoJava/idade.java
@@ -9,10 +9,23 @@ public class idade {
         System.out.println("Se tu for de maior e dos meus papai ");
         System.out.println("Vamos la agora digite e veremos");
 
+        System.out.println("Selecione uma opcao 1- + 2- -");
+        int opcao = scanner.nextInt();
+
+        System.out.println("Digite um classic numero papai:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Digite mais um numerozinho home");
+        int num2 = scanner.nextInt();
+
+        if (opcao == 1){
+            System.out.println("O resultado é " + (num1 + num2));
+        } else if (opcao == 2) {
+            System.out.println("O resultado é " + (num1 - num2));
+        }
+
         System.out.println("Digite sua idade meu patrao: ");
         int idade = scanner.nextInt();
-
-
 
         if (idade >= 18){
             System.out.println("Tu e fera meu guri");
