f04bb54629d932072559fb9515545a96abe64084
GustavoHeimburg <gutogustavogu@gmail.com>
2023-10-12 16:29:29 -0300

    Logicazinha pra nao perder costume

---
+++ src/CursoJava/Peso.java
@@ -0,0 +1,11 @@
+package CursoJava;
+
+import java.util.Scanner;
+
+public class Peso {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello, word");
+    }
+}
