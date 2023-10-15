65b752104eee922c5b426b4fe1d45076670b07f6
Gustavo Heimburg Pereira <133078374+GustavoHeimburg@users.noreply.github.com>
2023-10-15 14:38:32 -0300

    Atualizar o Peso.java

--- src/CursoJava/Peso.java
+++ src/CursoJava/Peso.java
@@ -6,6 +6,9 @@ public class Peso {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello, word");
+        System.out.println("Digite seu peso: ");
+         int peso = scanner.nextint();
+
+        System.out.println("Seu peso é " +peso);
     }
 }
